dc9dee637311cb5d7b731d59712bba2b63a5ca31
taikyunn <taichi_141@me.com>
2021-11-26 09:59:39 +0900

    Update main.go

--- main.go
+++ main.go
@@ -36,7 +36,7 @@ func errorHandler(w http.ResponseWriter, r *http.Request, status int) {
 }
 
 func helloWorld(w http.ResponseWriter, r *http.Request) {
-	html := "Hello World! Now updated with CircleCI v2"
+	html := "Hello World! Now updated with CircleCI v3"
 	if r.URL.Path != "/" {
 		errorHandler(w, r, http.StatusNotFound)
 		return
